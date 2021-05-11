ac751c7c7618722785ed23fff5e6b8d07e281256
Jonathan Garcia <714JonathanReneGarcia@gmail.com>
2021-05-10 19:02:29 -0700

    MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEM_SET = "MEM_SET";
+export const MEM_APPLY = "MEM_APPLY";
+export const MEM_CLEAR = "MEM_CLEAR";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperator = (operator) => {
+    return({type:CHANGE_OPERATION, payload:operator});
+}
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY});
+}
+
+export const memorySet = () => {
+    return({type:MEM_SET});
+}
+
+export const memoryApply = () => {
+    return({type:MEM_APPLY});
+}
+
+export const memoryClear = () => {
+    return({type:MEM_CLEAR});
 }
